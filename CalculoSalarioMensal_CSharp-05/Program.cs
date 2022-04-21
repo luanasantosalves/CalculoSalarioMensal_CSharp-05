34140791fd0c6a2c092af5843d6ab0cd214ae5a8
Luana <Luana@DESKTOP-ODA6GJK>
2022-04-21 17:50:51 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoSalarioMensal_CSharp-05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoSalarioMensal_CSharp_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int funcionario, horas;
+            double valorh;
+
+            Console.WriteLine("Qual o número do Funcionário por favor?");
+            funcionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantas horas de trabalho?");
+            horas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor por hora trabalhada?");
+            valorh = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = horas * valorh;
+
+            Console.WriteLine();
+            Console.WriteLine("Resultado:");
+            Console.WriteLine("NUMBER = " + funcionario);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
